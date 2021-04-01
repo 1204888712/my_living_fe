6798ab6a327a1a59e366d84e0967c8c55ed62db5
liguobiao <liguobiao@deepexi.com>
2021-04-01 20:25:19 +0800

    feature: 增加布局样式配置

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,21 +1,26 @@
+/*
+ * @LastEditors: liguobiao
+ * @LastEditTime: 2021-04-01 20:17:36
+ */
 import { Settings as LayoutSettings } from '@ant-design/pro-layout';
 
 const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  navTheme: 'light',
-  // 拂晓蓝
-  primaryColor: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'Ant Design Pro',
-  pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
+  "navTheme": "dark",
+  "primaryColor": "#1890ff",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "title": "Ant Design Pro",
+  "pwa": false,
+  "iconfontUrl": "",
+  "menu": {
+    "locale": true
+  },
+  "headerHeight": 48
 };
 
 export default Settings;
